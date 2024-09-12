95953882c696a16ff57562d63a9b3da22b96970b
Shadezjx <trandangminhhoang2005thd@gmail.com>
2024-09-13 01:42:49 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,12 @@
-import React, { useEffect, useState } from "react";
+// App.tsx
+import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import "../modal/Modal.css";
-
-
-function Modal() {
-  const [modal, setModal] = useState(false);
-  const [inputValue, setInputValue] = React.useState<string>('');
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log('Dữ liệu nhập vào:', inputValue);
-  };
-
-  const toggleModal = () => {
-    setModal(!modal);
-  };
-
-  if(modal) {
-    document.body.classList.add('active-modal')
-  } else {
-    document.body.classList.remove('active-modal')
-  }
-
-  return (
-    <>
-      <button onClick={toggleModal} className="btn-modal">
-        +New
-      </button>
-
-      {modal && (
-        <div className="modal">
-          <div onClick={toggleModal} className="overlay"></div>
-          <div className="modal-content">
-            <h2>Make your todo</h2>
-            <form onSubmit={handleSubmit}>
-          <label htmlFor="inputField"></label>
-          <input
-            type="text"
-            id="inputField"
-            value={inputValue}
-            onChange={handleChange}
-          />
-          <button type="submit" onClick={toggleModal}>Gửi</button>
-        </form>
-
-            <button className="close-modal" onClick={toggleModal}>
-            X
-            </button>
-          </div>
-        </div>
-      )}
-    </>
-  );
-}
+import Modal from '../modal/Modal.tsx'; // Đảm bảo rằng bạn import Modal đúng cách
 
 const client = generateClient<Schema>();
 
@@ -68,35 +14,48 @@ function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+
+    return () => subscription.unsubscribe(); // Cleanup subscription on component unmount
   }, []);
   
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+  function addTodo(content: string) {
+    client.models.Todo.create({ content })
+      .then(() => {
+        // Reload todos after adding a new one
+        client.models.Todo.observeQuery().subscribe({
+          next: (data) => setTodos([...data.items]),
+        });
+      });
   }
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
+  }
+
   return (
-        
     <Authenticator>
       {({ signOut, user }) => (
-    <main>
-      <h1 >My todos</h1>
-      <Modal></Modal>
-      <ul>
-        {todos.map((todo) => (
-        <li onClick={() => deleteTodo(todo.id)} key={todo.id}>hello</li>
-        ))}
-      </ul>
-      <div>
-         Try creating a new todo.
-      </div>
-      <button onClick={signOut}>Sign out</button>
-      <h1>{user?.signInDetails?.loginId}'s todo</h1>
-    </main>
-        
+        <main>
+          <h1>My todos</h1>
+          <Modal addTodo={addTodo} />
+          <ul>
+            {todos.map((todo) => (
+              <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
+                {todo.content}
+              </li>
+            ))}
+          </ul>
+          <div>
+            Try creating a new todo.
+          </div>
+          <button onClick={signOut}>Sign out</button>
+          <h1>{user?.signInDetails?.loginId}'s todo</h1>
+        </main>
       )}
-      </Authenticator>
+    </Authenticator>
   );
 }
 
